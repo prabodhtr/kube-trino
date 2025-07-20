810faf1a634dc1fb3f1573effef353f5ee9ef4e8
prabodhtr <prabodhtr@gmail.com>
2025-07-20 15:03:04 +0530

    Push kube trino setup files

---
+++ README.md
@@ -0,0 +1,139 @@
+### Steps to start trino cluster:
+1. start docker
+2. create local kubernetes cluster using kind
+    - `kind create cluster test-cluster`
+    - `kind get clusters`
+3. Add `trino`, and `metrics-server` helm charts onto helm repo
+    - `helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/`
+    - `helm repo add tirno https://trinodb.github.io/charts`
+    - `helm repo list`
+    - `helm search repo` -- get all helm charts available
+   
+4. Install metric-server for exposing metrics ( required for HPA ). Note that it should be installed in `kube-system` namespace
+    - `helm upgrade --install --set args={--kubelet-insecure-tls} metrics-server metrics-server/metrics-server --namespace kube-system`
+    - `kubectl get all --namespace kube-system`
+
+5. Change directory to `~/projects/kube-trino` and start `kubernetes operator service`. The service monitors coordinator scaling and spawns worker deployments as needed
+    - `kopf run --namespace default --verbose operator.py`
+
+6. Start a trino cluster. Install it from local chart repo for testing with values to override
+    - `helm install resil --values trino-override.yaml ../charts/charts/cluster-init`
+    - `kubectl get all -o wide` -- default namespace
+
+7. Check operator service logs to see if worker nodes and services are created as needed
+    - `kubectl get all -o wide` -- default namespace 
+
+6. Figure out service port for trino coordinator service and do port forwarding
+    - `kubectl port-forward service/resil-1-coordinator 8090:8090`
+
+7. Run simple query and check response to see if coordinator and nodes are tied
+    - `trino http://localhost:8090`
+    - `select * from system.runtime.nodes`
+
+
+### Setup gateway for HA cluster mode
+1. Start postgresql container for gateway backend storage
+    - `docker volume rm kube-trino_postgres-data` (delete persistent postgres docker volume if need be)
+    - `docker-compose -f compose.yaml start`
+    
+
+2. Create configmap from default routing rule to be provided to gateway
+    - `kubectl create configmap routing-rules-config --from-file=routing-rules.yml`
+
+3. Start gateway using helm
+    - `helm install gateway --values gateway-override.yaml ../charts/charts/gateway`
+
+4. Port forward gateway service port to localhost
+    - `kubectl port-forward service/trino-gateway 8080:8080`
+
+5. Navigate to UI and add backends and routing rules, if not present, according to the trino cluster you have setup
+    - `http://localhost:8080`
+    - Backend cluster must proxy to `http://<coordinator_service_name>:8090`
+    - Routing logic must point to backend cluster name created above
+
+6. Run simple query and check response from CLI
+    - `trino http://localhost:8080`
+    - `select * from system.runtime.nodes`
+
+7. Navigate back to UI to see the query details
+
+
+### Running kopf in kubernetes
+- create cluster config maps:
+  - `kubectl create configmap kopf-operator-code --from-file=operator.py`
+  - `kubectl create configmap trino-override --from-file ../trino-override.yaml`
+  - `kubectl create configmap post-construct-templates --from-file=../../charts/charts/post-construct/templates/`
+  - `kubectl create configmap post-construct-chart --from-file=../../charts/charts/post-construct/`
+- The init container of kopf in manifest.yaml will copy these configs to a common directory which is used by kopf during helm install
+- apply rbac.yaml
+  - `kubectl apply -f rbac.yaml`
+- apply manifest.yaml
+  - `kubectl apply -f manifest.yaml`
+- helm and kubectl sources the same kubeconfig present in ~/.kube/config by default for registering resources. We copy this config from localhost
+    to the pod in kopf's Dockerfile so that they both refer to same kind cluster
+- NOTE: incase kind cluster is restarted, update the config file `kopf/config` with that present in `~/.kube/config` before recreating the kopf container
+
+NOTE: delete cluster, gateway and then kopf. Else pod will never be deleted
+
+NOTE: we cannot use hpa to manage coordinator scaling because:
+1. Although scaling up is stateless, scale down should only be done by deleting coordinators WITHOUT any running queries
+2. On detecting scale down, hpa will directly change deployment's 'spec.replica', allowing controller to drop last spawned pod(usually)
+3. although kopf can intercept this change from hpa and deployment, it cannot stop/block the pod deletion by controller as the change would have already been implemented by the time kopf detects and intercepts the event
+4. Even if kopf event handler reverts the replica count, it would only make a new pod spawn AFTER the pod deletion was initiated by controller, causing query failures in pod that was dropped
+5. Hence, need to allow kopf itself to manage scaling by polling trino jmx metrics exposed to prometheus server
+
+
+### Bring up prometheus server and grafana for scaling and monitoring
+- Add prometheus helm repo
+  - `helm repo add prometheus-community https://prometheus-community.github.io/helm-charts`
+  - `helm repo upgrade`
+  - `helm install prometheus prometheus-community/prometheus`
+  - `kubectl port-forward service/prometheus-server 8080:80`
+- Goto http://localhost:8080 and fire promQL queries
+  - Refer `https://medium.com/@gayatripawar401/deploy-prometheus-and-grafana-on-kubernetes-using-helm-5aa9d4fbae66`
+
+
+### Enable JMX in trino coordinators and make prometheus server scrape metrics 
+- Enable jmx catalog, jmx server and jmx prometheus exporter as provided in trino-override.yaml
+- Add prometheus scrape path/port as annotations in coordinator annotations for prometheus server to scrape from
+  - Refer `https://github.com/prometheus-community/helm-charts/tree/main/charts/prometheus#scraping-pod-metrics-via-annotations`
+- Install trino coordinator using
+  - `helm install resil --values trino-override.yaml ../charts/charts/cluster-init`
+- coordinator pod will have two containers with one being 'trino-coordinator' and the other 'jmx-exporter'
+- Check logs of each to make sure both are working fine
+  - `kubectl describe pod/<pod>`
+  - `kubectl logs -f pod<pod> <container_name>`
+- Port forward jmx-exporter container port to confirm that jmx metrics are exposed
+  - `kubectl port-forward pod/<coordinator_pod> 5556:5556`
+- Goto `http://localhost:5556`
+- Get a metric and run promQL query in prometheus UI to confirm that metrics are scraped
+
+
+### Useful kube commands
+- kubectl component_types:
+  - `pod`
+  - `service`
+  - `deployment`
+  - `hpa`
+  
+- Get logs of a component
+  - `kubectl logs <component_type/podname>`
+  - `kubectl logs -f <component_type/podname>` -- tail logs
+  - `kubectl logs -f <component_type/podname> <initContainer command name>` -- check init script logs
+- Get component details
+  - `kubectl describe <component_type> <component_name>` -- if pod has not started due to some reason
+- Get component manifest
+  - `kubectl get <component_type>/<component_name> -o yaml`
+- Debug template issues
+  - `helm install <name> --values <overriden_values_file> <helm_template_path> --debug`
+  - `helm install <name> --values <overriden_values_file> <helm_template_path> --dry-run`
+  
+
+
+[//]: # (TODO)
+1. Run a scheduled process from kopf to scrape running/started/queued query metric prometheus server for each cluster_instance by filtering on label
+2. Scale up if threshold is met
+3. Scale down if value is below threshold for a time(use aggregated metric, avg over 1m)
+    - could use 'java_lang_Memory_HeapMemoryUsage_used'/java_lang_Memory_HeapMemoryUsage_max and 'trino_memory_MemoryPool_FreeBytes'
+    - use metric to decide which cluster_instance to uninstall
+
---
+++ kopf/operator.py
@@ -0,0 +1,289 @@
+import threading
+from random import randint
+
+import kopf
+import kubernetes
+import logging as logger
+import subprocess
+import requests
+from requests.auth import HTTPBasicAuth
+
+import asyncio
+
+lock = threading.Lock()
+
+logging_task = None
+
+clusters = {}
+
+def get_new_cluster_instance(pod_labels):
+    cluster_group = pod_labels["cluster-group"]
+
+    if clusters.get(cluster_group) is None:
+        clusters[cluster_group] = []
+
+    while True:
+        cluster_id = randint(1, 100)
+        new_cluster = f"{cluster_group}-{cluster_id}"
+        if not clusters.get(cluster_group).__contains__(new_cluster):
+            break
+
+    return new_cluster
+
+@kopf.on.startup()
+def discover_running_clusters(**kwargs):
+    from kubernetes import config
+    config.load_incluster_config()
+
+    api = kubernetes.client.CoreV1Api()
+    logger.info("Starting cluster discovery!")
+    pods = api.list_namespaced_pod(namespace='default', label_selector='app.kubernetes.io/component=coordinator')
+    for pod in pods.items:
+        labels = pod.metadata.labels
+        cluster_group = labels['cluster-group']
+        cluster_instance = labels['cluster-instance']
+        if cluster_group is not None and cluster_instance is not None:
+            with lock:
+                if clusters.get(cluster_group) is None:
+                    clusters[cluster_group] = [cluster_instance]
+                elif not clusters.get(cluster_group).__contains__(cluster_instance):
+                    clusters.get(cluster_group).append(cluster_instance)
+
+    logger.info("Cluster discovery complete!")
+
+@kopf.on.startup()
+async def run_background_timer(**kwargs):
+    global logging_task
+    loop = asyncio.get_running_loop()
+    logging_task = loop.create_task(background_loop())
+
+async def background_loop():
+    while True:
+        logger.info(f"Active clusters: {clusters}")
+        await asyncio.sleep(10)
+
+@kopf.on.cleanup()
+def on_cleanup(**_):
+    global logging_task
+    if logging_task:
+        logger.info("Shutting down background logging_task")
+        logging_task.cancel()  # Graceful shutdown
+
+
+@kopf.on.create(version='v1', kind='Pod', labels={'app.kubernetes.io/component': 'coordinator'})
+def install_cluster_components(spec, meta, namespace, **kwargs):
+    labels = meta.get('labels', {})
+    cluster_group = labels["cluster-group"]
+
+    name = meta['name']
+    api = kubernetes.client.CoreV1Api()
+
+    cluster_instance = get_new_cluster_instance(labels)
+    label_patch = {
+        "metadata": {
+            "labels": {
+                "cluster-instance": cluster_instance
+            }
+        }
+    }
+
+    logger.info(f"Assigning coordinator pod {name} to cluster instance '{cluster_instance}'")
+    api.patch_namespaced_pod(name=name, namespace=namespace, body=label_patch)
+
+    # create service coordinator, deployment worker, service worker and worker autoscaler with clusterInstance value set
+    helm_cmd = [
+        "helm", "install", f"{cluster_instance}",
+        "--values", "/app/trino-override.yaml",
+        "/post-construct/chart",
+        "--namespace", "default",
+        "--set", f"clusterInstance={cluster_instance}",
+        "--set", f"coordinator.host={cluster_instance}-coordinator"
+    ]
+
+    logger.info(f"Installing components of coordinator {name} in cluster '{cluster_instance}'")
+    try:
+        logger.info(f"Running Helm command: {' '.join(helm_cmd)}")
+        result = subprocess.run(helm_cmd, check=True, capture_output=True, text=True)
+        logger.info(f"Helm output: {result.stdout}")
+
+        with lock:
+            clusters.get(cluster_group).append(cluster_instance)
+
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Helm install failed: {e.stderr}")
+        raise kopf.TemporaryError("Failed to install Helm chart", delay=30)
+
+
+
+@kopf.on.create(version='v1', kind='Service', labels={'app.kubernetes.io/component': 'coordinator'})
+def register_in_gateway(spec, meta, namespace, **kwargs):
+
+    labels = meta.get('labels', {})
+    cluster_group = labels["cluster-group"]
+    cluster_instance = labels["cluster-instance"]
+    service_name = meta['name']
+    service_port = spec['ports'][0]['port']
+
+    logger.info(f"Registering cluster '{cluster_instance}' in gateway")
+    try:
+        resp = requests.post(
+            url="http://trino-gateway:8080/entity?entityType=GATEWAY_BACKEND",
+            json={
+                "name": cluster_instance,
+                "proxyTo": f'http://{service_name}:{service_port}',
+                "active": "true",
+                "routingGroup": f'{cluster_instance}'
+            }
+        )
+        logger.info(f"Response code: {resp.status_code}; Body: {resp.text}")
+    except Exception as e:
+        logger.error(f"service registration failed: {e}")
+        raise kopf.TemporaryError(f"Failed to register against gateway for '{cluster_instance}'", delay=30)
+
+    logger.info(f"Updating gateway rule to route traffic to cluster '{cluster_instance}'")
+    resp = requests.post(
+        url="http://trino-gateway:8080/webapp/updateRoutingRules",
+        json={
+            "name": f"route_to_{cluster_group}",
+            "description": f'Rule to route traffic to active instance of {cluster_group}',
+            "priority": 1,
+            "condition": f'request.getHeader("X-Trino-Client-Tags") contains "cg:{cluster_group}"',
+            "actions": [f'result.put("routingGroup", "{cluster_instance}")']
+        }
+    )
+    try:
+        logger.info(f"Response code: {resp.status_code}; Body: {resp.text}")
+    except Exception as e:
+        logger.error(f"Failed to update routing rules for cluster group {cluster_group}: {e}")
+        raise kopf.TemporaryError(f"Failed to failed to update routing rules for cluster-group '{cluster_group}'", delay=30)
+
+@kopf.on.delete(version='v1', kind='Pod', labels={'app.kubernetes.io/component': 'coordinator'})
+def delete_cluster_components(spec, meta, namespace, **kwargs):
+    labels = meta.get('labels', {})
+    cluster_group = labels["cluster-group"]
+    cluster_instance = labels["cluster-instance"]
+
+    uninstall_cluster(cluster_group, cluster_instance)
+
+
+def uninstall_cluster(cluster_group, cluster_instance):
+    helm_cmd = [
+        "helm", "uninstall", f"{cluster_instance}"
+    ]
+    logger.warning(f"Un-installing components of cluster {cluster_instance}")
+    try:
+        logger.info(f"Running Helm command: {' '.join(helm_cmd)}")
+        result = subprocess.run(helm_cmd, check=True, capture_output=True, text=True)
+        logger.info(f"Helm output: {result.stdout}")
+        try:
+            with lock:
+                clusters.get(cluster_group).remove(cluster_instance)
+        except Exception as e:
+            logger.error(f"Error removing cluster instance {cluster_instance} from clusters: {e.__str__()}")
+        return "success"
+
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Helm command failed: {e.stderr}")
+        if str(e.stderr).__contains__("Release not loaded"):
+            logger.warning("Ignoring error as release might have been deleted manually!")
+        else:
+            raise kopf.TemporaryError("Failed to run Helm command", delay=30)
+
+# @kopf.on.field(kind="HorizontalPodAutoscaler", version='v1', labels={'app.kubernetes.io/component': 'coordinator-hpa'}, field='status.desiredReplicas')
+# def control_hpa_scaling(old, new, spec, labels, name, namespace, **kwargs):
+#     api = kubernetes.client.AutoscalingV1Api()
+#     if old is not None and new < old:
+#         actual_min_replicas = spec["minReplicas"]
+#         logger.info(f"current min replica: {actual_min_replicas}")
+#         # revert scale down event to let kopf perform intelligent cluster uninstall
+#         logger.info(f"Scale down detected for {name}: {old} → {new}")
+#         # Patch HPA spec to bump minReplicas
+#         logger.info(f"Blocking hpa's scale down by setting minReplica as {old} to allow intelligent scale down from kopf!")
+#         api.patch_namespaced_horizontal_pod_autoscaler(
+#             name=name,
+#             namespace=namespace,
+#             body={"spec": {"minReplicas": old}})
+#
+#         # Optionally: remove the least-used pod manually
+#         # get the cluster group whose deployment got scaled down
+#         cluster_group = labels.get('cluster-group', {})
+#         logger.info(f"Performing intelligent scale down for cluster-group {cluster_group}")
+#
+#         # get number of pods to scale down
+#         pods_to_scale_down = old - new
+#
+#         # iterate through list of cluster-instances under cluster group and find one that has no queries running. Perform helm uninstall for that instance
+#         for cluster_instance in clusters[cluster_group]:
+#             coordinator_url = f"{cluster_instance}-coordinator:8090"
+#             resp = requests.get(url=f"http://{coordinator_url}/ui/api/stats", auth=HTTPBasicAuth("admin", ""))
+#             logger.info(f"stats of cluster {cluster_instance}: {resp.text}")
+#             # "runningQueries":0,"blockedQueries":0,"queuedQueries":0,"activeCoordinators":1,"activeWorkers":1,"runningDrivers":0,"totalAvailableProcessors":8,"reservedMemory":0.0,"totalInputRows":0,"totalInputBytes":0,"totalCpuTimeSecs":0}
+#             if resp.status_code == 200:
+#                 resp = resp.json()
+#                 if resp["runningQueries"] != 0:
+#                     continue
+#                 uninstall_cluster(cluster_group, cluster_instance)
+#                 pods_to_scale_down = pods_to_scale_down - 1
+#
+#             # perform till required pods are removed
+#             if pods_to_scale_down <= 0:
+#                 break
+#
+#         # if not enough instances are available to scale down, patch deployment with replica count that matches active instances and wait for 1 minute for hpa to trigger again
+#         logger.info(f"Finishing scale down handle by setting hpa of {name} with minReplicas: {len(clusters.get(cluster_group))}")
+#         api.patch_namespaced_horizontal_pod_autoscaler(
+#             name=name,
+#             namespace=namespace,
+#             body={"spec": {"minReplicas": len(clusters.get(cluster_group))}})
+
+# @kopf.on.field('apps', 'v1', 'deployments', labels={'app.kubernetes.io/component': 'coordinator'}, field='spec.replicas')
+# def on_deployment_scaled_down(old, new, name, labels, namespace, **kwargs):
+#
+#     api = kubernetes.client.AppsV1Api()
+#     if old is not None and new < old:
+#         # revert scale down event to let kopf perform intelligent cluster uninstall
+#         logger.info(f"Scale down detected for {name}: {old} → {new}")
+#
+#         logger.info(f"Blocking hpa's scale down event to allow intelligent scale down from kopf!")
+#         replicas_patch = {
+#             "spec": {
+#                 "replicas": old
+#             }
+#         }
+#         api.patch_namespaced_deployment(name=name, namespace=namespace, body=replicas_patch)
+#
+#         # Optionally: remove the least-used pod manually
+#         # get the cluster group whose deployment got scaled down
+#         cluster_group = labels.get('cluster-group', {})
+#         logger.info(f"Performing intelligent scale down for cluster-group {cluster_group}")
+#
+#         # get number of pods to scale down
+#         pods_to_scale_down = old - new
+#
+#         # iterate through list of cluster-instances under cluster group and find one that has no queries running. Perform helm uninstall for that instance
+#         for cluster_instance in clusters[cluster_group]:
+#             coordinator_url = f"{cluster_instance}-coordinator:8090"
+#             resp = requests.get(url=f"http://{coordinator_url}/ui/api/stats", auth=HTTPBasicAuth("admin", ""))
+#             logger.info(f"stats of cluster {cluster_instance}: {resp.text}")
+#             # "runningQueries":0,"blockedQueries":0,"queuedQueries":0,"activeCoordinators":1,"activeWorkers":1,"runningDrivers":0,"totalAvailableProcessors":8,"reservedMemory":0.0,"totalInputRows":0,"totalInputBytes":0,"totalCpuTimeSecs":0}
+#             if resp.status_code == 200:
+#                 resp = resp.json()
+#                 if resp["runningQueries"] != 0:
+#                     continue
+#                 uninstall_cluster(cluster_group, cluster_instance)
+#                 pods_to_scale_down = pods_to_scale_down - 1
+#
+#             # perform till required pods are removed
+#             if pods_to_scale_down <= 0:
+#                 break
+#
+#         # if not enough instances are available to scale down, patch deployment with replica count that matches active instances and wait for 1 minute for hpa to trigger again
+#         current_replica_count = len(clusters.get(cluster_group))
+#         replicas_patch = {
+#             "spec": {
+#                 "replicas": current_replica_count
+#             }
+#         }
+#
+#         logger.info(f"Patching replica count of deployment {name} to '{current_replica_count}'")
+#         api.patch_namespaced_deployment(name=name, namespace=namespace, body=replicas_patch)
